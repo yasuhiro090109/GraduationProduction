73daa31904187ebcb7d020a3a8017f1a942e0b81
igarashi nobuyuki <nobujojo.0501@hotmail.co.jp>
2014-12-18 12:12:53 +0900

    Camera Controll from Mouse

--- Camera.cpp
+++ Camera.cpp
@@ -5,7 +5,15 @@ void CCamera::Reset() {
 }
 
 // ƒJƒƒ‰‚ÌˆÚ“®
-void CCamera::Move() {
-	Rotation=Oculus[0].Rotation;
-	Position+=Key[0].Direction().XFY(0)*Camera->Rotation*0.2f;
+void CCamera::Move()
+{
+	if( Oculus[0] ) {
+		Rotation = Oculus[0].Rotation;
+		Position += Key[0].Direction().XFY( 0 )*Camera->Rotation*0.2f;
+	} else {
+		CVector v = MouseState[0].Movement;
+		Rotation *= CQuaternion( AxisX(), MouseState[0].Movement.Y*-0.001f );
+		Rotation *= CQuaternion( AXIS_Y, MouseState[0].Movement.X*0.001f );
+	}
+
 }
