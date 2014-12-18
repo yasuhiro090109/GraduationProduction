5dab7b55ab3d599af8d3c9d2655e82548b203851
yasuhiro <yasugigabaitointel@gmail.com>
2014-12-18 15:41:23 +0900

    ターゲット位置に移動するようにしました。

--- Player.cpp
+++ Player.cpp
@@ -4,10 +4,17 @@
 void CPlayer::Reset() {
 	Model=GetModel(Random(0, 1)<0.5f?L"PenguinLight.x":L"BearLight.x");
 	Rotation=RotationY(Random(0, 1));
+	EnemyList.Apply([&](CMover* e){
+		target = e;
+	});
 }
 
 // Ž©‹@‚ÌˆÚ“®
 void CPlayer::Move() {
-	Rotation*=RotationY(0.01f);
+	//Rotation*=RotationY(0.01f);
+	Rotation = CQuaternion(AXIS_Z,target->Position.FFY(0)-Position);
+	//Velocity = Normalize( Dot(Velocity,target->Position))*0.1f;
+	Velocity = Normalize(target->Position.FFY(0) - Position)*0.1f;
+	Position += Velocity;
 }
 
